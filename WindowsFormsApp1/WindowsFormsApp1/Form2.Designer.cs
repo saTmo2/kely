0866fb7192290c4ea0c6564f40620afd5580fc10
saTmo2 <santiago.marin415@pascualbravo.edu.co>
2024-10-30 19:37:09 -0500

    Add files via upload
    nuevo

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPregunta = new System.Windows.Forms.Label();
+            this.rbOpcion1 = new System.Windows.Forms.RadioButton();
+            this.rbOpcion2 = new System.Windows.Forms.RadioButton();
+            this.rbOpcion3 = new System.Windows.Forms.RadioButton();
+            this.rbOpcion4 = new System.Windows.Forms.RadioButton();
+            this.rbVerdadero = new System.Windows.Forms.RadioButton();
+            this.rbFalso = new System.Windows.Forms.RadioButton();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.lblPuntaje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPregunta
+            // 
+            this.lblPregunta.AutoSize = true;
+            this.lblPregunta.Location = new System.Drawing.Point(40, 71);
+            this.lblPregunta.Name = "lblPregunta";
+            this.lblPregunta.Size = new System.Drawing.Size(78, 16);
+            this.lblPregunta.TabIndex = 0;
+            this.lblPregunta.Text = "pregunta hp";
+            // 
+            // rbOpcion1
+            // 
+            this.rbOpcion1.AutoSize = true;
+            this.rbOpcion1.Location = new System.Drawing.Point(218, 71);
+            this.rbOpcion1.Name = "rbOpcion1";
+            this.rbOpcion1.Size = new System.Drawing.Size(103, 20);
+            this.rbOpcion1.TabIndex = 1;
+            this.rbOpcion1.TabStop = true;
+            this.rbOpcion1.Text = "radioButton1";
+            this.rbOpcion1.UseVisualStyleBackColor = true;
+            // 
+            // rbOpcion2
+            // 
+            this.rbOpcion2.AutoSize = true;
+            this.rbOpcion2.Location = new System.Drawing.Point(218, 97);
+            this.rbOpcion2.Name = "rbOpcion2";
+            this.rbOpcion2.Size = new System.Drawing.Size(103, 20);
+            this.rbOpcion2.TabIndex = 2;
+            this.rbOpcion2.TabStop = true;
+            this.rbOpcion2.Text = "radioButton2";
+            this.rbOpcion2.UseVisualStyleBackColor = true;
+            // 
+            // rbOpcion3
+            // 
+            this.rbOpcion3.AutoSize = true;
+            this.rbOpcion3.Location = new System.Drawing.Point(218, 123);
+            this.rbOpcion3.Name = "rbOpcion3";
+            this.rbOpcion3.Size = new System.Drawing.Size(103, 20);
+            this.rbOpcion3.TabIndex = 3;
+            this.rbOpcion3.TabStop = true;
+            this.rbOpcion3.Text = "radioButton3";
+            this.rbOpcion3.UseVisualStyleBackColor = true;
+            // 
+            // rbOpcion4
+            // 
+            this.rbOpcion4.AutoSize = true;
+            this.rbOpcion4.Location = new System.Drawing.Point(218, 149);
+            this.rbOpcion4.Name = "rbOpcion4";
+            this.rbOpcion4.Size = new System.Drawing.Size(103, 20);
+            this.rbOpcion4.TabIndex = 4;
+            this.rbOpcion4.TabStop = true;
+            this.rbOpcion4.Text = "radioButton4";
+            this.rbOpcion4.UseVisualStyleBackColor = true;
+            // 
+            // rbVerdadero
+            // 
+            this.rbVerdadero.AutoSize = true;
+            this.rbVerdadero.Location = new System.Drawing.Point(218, 71);
+            this.rbVerdadero.Name = "rbVerdadero";
+            this.rbVerdadero.Size = new System.Drawing.Size(97, 20);
+            this.rbVerdadero.TabIndex = 5;
+            this.rbVerdadero.TabStop = true;
+            this.rbVerdadero.Text = "Verdadero";
+            this.rbVerdadero.UseVisualStyleBackColor = true;
+            this.rbVerdadero.Visible = false; // Inicialmente oculto
+            // 
+            // rbFalso
+            // 
+            this.rbFalso.AutoSize = true;
+            this.rbFalso.Location = new System.Drawing.Point(218, 97);
+            this.rbFalso.Name = "rbFalso";
+            this.rbFalso.Size = new System.Drawing.Size(62, 20);
+            this.rbFalso.TabIndex = 6;
+            this.rbFalso.TabStop = true;
+            this.rbFalso.Text = "Falso";
+            this.rbFalso.UseVisualStyleBackColor = true;
+            this.rbFalso.Visible = false; // Inicialmente oculto
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(228, 214);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 7;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // lblPuntaje
+            // 
+            this.lblPuntaje.AutoSize = true;
+            this.lblPuntaje.Location = new System.Drawing.Point(529, 75);
+            this.lblPuntaje.Name = "lblPuntaje";
+            this.lblPuntaje.Size = new System.Drawing.Size(51, 16);
+            this.lblPuntaje.TabIndex = 8;
+            this.lblPuntaje.Text = "Puntaje";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPuntaje);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.rbFalso);
+            this.Controls.Add(this.rbVerdadero);
+            this.Controls.Add(this.rbOpcion4);
+            this.Controls.Add(this.rbOpcion3);
+            this.Controls.Add(this.rbOpcion2);
+            this.Controls.Add(this.rbOpcion1);
+            this.Controls.Add(this.lblPregunta);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPregunta;
+        private System.Windows.Forms.RadioButton rbOpcion1;
+        private System.Windows.Forms.RadioButton rbOpcion2;
+        private System.Windows.Forms.RadioButton rbOpcion3;
+        private System.Windows.Forms.RadioButton rbOpcion4;
+        private System.Windows.Forms.RadioButton rbVerdadero;
+        private System.Windows.Forms.RadioButton rbFalso;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label lblPuntaje;
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        private int puntaje = 0;
+        private int preguntaActual = 0;
+
+        // Preguntas de selección múltiple
+        private string[] preguntas = {
+            "¿Cuál es la comida favorita de Santiago?",
+            "¿Cuál es el tamaño de la verga de Santi?",
+            "¿Cuál es la moto de Santi?",
+            "¿Quién es la suegra?",
+            "¿Cuál es el mejor pasatiempo?"
+        };
+
+        // Opciones de selección múltiple
+        private string[,] opciones = {
+            { "todas las anteriores", "frijoles", "hamburguesa", "perro" },
+            { "3", "16", "5", "11" },
+            { "kaffir", "boxer ct 1200r", "SZ-r 150", "nkFir" },
+            { "adelaida osorio", "glenis marin", "martha", "tereza pino" },
+            { "pasar tiempo con santi", "sexo", "gum a muerte", "comer" }
+        };
+
+        // Respuestas correctas para selección múltiple
+        private int[] respuestasCorrectas = { 0, 1, 2, 0, 0 };
+
+        // Preguntas de verdadero y falso
+        private string[] preguntasVF = {
+            "¿La Tierra es plana?", // Pregunta de verdadero/falso
+            "¿El agua es líquida a temperatura ambiente?" // Pregunta de verdadero/falso
+        };
+
+        // Respuestas correctas para verdadero/falso
+        private bool[] respuestasVerdaderas = { false, true };
+
+        public Form2()
+        {
+            InitializeComponent();
+            CargarPregunta();
+        }
+
+        private void CargarPregunta()
+        {
+            // Verificar si hay más preguntas de selección múltiple
+            if (preguntaActual < preguntas.Length)
+            {
+                lblPregunta.Text = preguntas[preguntaActual];
+                rbOpcion1.Visible = true;
+                rbOpcion2.Visible = true;
+                rbOpcion3.Visible = true;
+                rbOpcion4.Visible = true;
+
+                rbOpcion1.Text = opciones[preguntaActual, 0];
+                rbOpcion2.Text = opciones[preguntaActual, 1];
+                rbOpcion3.Text = opciones[preguntaActual, 2];
+                rbOpcion4.Text = opciones[preguntaActual, 3];
+
+                // Ocultar botones de verdadero y falso
+                rbVerdadero.Visible = false;
+                rbFalso.Visible = false;
+            }
+            // Verificar si hay más preguntas de verdadero/falso
+            else if (preguntaActual - preguntas.Length < preguntasVF.Length)
+            {
+                lblPregunta.Text = preguntasVF[preguntaActual - preguntas.Length];
+
+                // Ocultar opciones de selección múltiple
+                rbOpcion1.Visible = false;
+                rbOpcion2.Visible = false;
+                rbOpcion3.Visible = false;
+                rbOpcion4.Visible = false;
+
+                // Mostrar botones de verdadero y falso
+                rbVerdadero.Visible = true;
+                rbFalso.Visible = true;
+
+                rbVerdadero.Text = "Verdadero";
+                rbFalso.Text = "Falso";
+            }
+            else
+            {
+                MessageBox.Show($"Juego terminado. Tu puntaje final es: {puntaje}");
+                this.Close();
+            }
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            int respuestaSeleccionada = -1;
+
+            // Obtener respuesta de selección múltiple
+            if (rbOpcion1.Visible && rbOpcion1.Checked) respuestaSeleccionada = 0;
+            else if (rbOpcion2.Visible && rbOpcion2.Checked) respuestaSeleccionada = 1;
+            else if (rbOpcion3.Visible && rbOpcion3.Checked) respuestaSeleccionada = 2;
+            else if (rbOpcion4.Visible && rbOpcion4.Checked) respuestaSeleccionada = 3;
+
+            // Verificar respuesta de selección múltiple
+            if (respuestaSeleccionada != -1 && preguntaActual < preguntas.Length && respuestaSeleccionada == respuestasCorrectas[preguntaActual])
+            {
+                puntaje++;
+            }
+            // Verificar respuesta de verdadero/falso
+            else if (preguntaActual >= preguntas.Length && preguntaActual - preguntas.Length < preguntasVF.Length)
+            {
+                if (rbVerdadero.Checked && respuestasVerdaderas[preguntaActual - preguntas.Length])
+                {
+                    puntaje++;
+                }
+                else if (rbFalso.Checked && !respuestasVerdaderas[preguntaActual - preguntas.Length])
+                {
+                    puntaje++;
+                }
+            }
+
+            preguntaActual++;
+            CargarPregunta();
+            lblPuntaje.Text = $"Puntaje: {puntaje}";
+        }
+    }
+}
+
